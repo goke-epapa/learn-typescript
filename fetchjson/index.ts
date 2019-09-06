943cbfc971c88f47d09a9c4f034c979376759323
Adegoke Obasa <adegokeobasa@gmail.com>
2019-09-06 08:49:30 +0200

    introduce interface and fix bugs

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,15 +2,22 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+// Interface in typescript
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  const todo = response.data;
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+  const todo = response.data as Todo;
+  const ID = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
 
   console.log(`
     The Todo with ID: ${ID}
     Has a title of: ${title}
-    Is it finished? ${finished}
+    Is it finished? ${completed}
   `);
 });
