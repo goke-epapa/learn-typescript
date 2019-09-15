a1a1172f49a9682db108d6ad8c8340c4bf5a5bed
Adegoke Obasa <adegokeobasa@gmail.com>
2019-09-15 10:49:09 +0200

    implement sort for LinkedList

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,9 +1,19 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([50, 10, 3, -5, 0]);
 const charactersCollection = new CharactersCollection('adegokeobasa');
 const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(sorter.collection);
+
+const linkedList = new LinkedList();
+linkedList.add(50);
+linkedList.add(-100);
+linkedList.add(5100);
+linkedList.add(0);
+const llSorter = new Sorter(linkedList);
+llSorter.sort();
+linkedList.print();
