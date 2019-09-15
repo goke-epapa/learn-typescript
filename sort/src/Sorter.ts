fdf0f682b879fe6930998c665ebb7c80c0828af1
Adegoke Obasa <adegokeobasa@gmail.com>
2019-09-15 18:00:26 +0200

    refactor sorter to use abstract class

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,20 +1,19 @@
-import { NumbersCollection } from './NumbersCollection';
-
 export interface Sortable {
   length: number;
   compare(leftIndex: number, rightIndex: number): boolean;
   swap(leftindex: number, rightIndex: number): void;
 }
-export class Sorter {
-  constructor(public collection: Sortable) {}
+
+export abstract class Sorter implements Sortable {
+  abstract length: number;
+  abstract swap(leftIndex: number, rightIndex: number): void;
+  abstract compare(leftIndex: number, rightIndex: number): boolean;
 
   sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i - 1; j++) {
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,19 +1,16 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([50, 10, 3, -5, 0]);
 const charactersCollection = new CharactersCollection('adegokeobasa');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(sorter.collection);
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(50);
 linkedList.add(-100);
 linkedList.add(5100);
 linkedList.add(0);
-const llSorter = new Sorter(linkedList);
-llSorter.sort();
+linkedList.sort();
 linkedList.print();
