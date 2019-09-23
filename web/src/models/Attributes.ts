1221a758efdeba1c9f714d06cfa29261f7795de2
Adegoke Obasa <adegokeobasa@gmail.com>
2019-09-23 23:20:33 +0200

    separate concerns + advanced generics

---
+++ web/src/models/Attributes.ts
@@ -0,0 +1,18 @@
+import {UserProps} from './User';
+
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
+  }
+
+  set(update: T): void {
+    Object.assign(this.data, update);
+  }
+}
+
+const attrs =
+    new Attributes<UserProps>({id: 5, name: 'Adegoke Obasa', age: 20});
+
+const id = attrs.get('age');
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,59 +1,16 @@
-import axios, { AxiosResponse } from 'axios';
 
-interface UserProps {
-	id?: number;
-	name?: string;
-	age?: number;
+import {Eventing} from './Eventing';
+import {Sync} from './Sync';
+
+export interface UserProps {
+  id?: number;
+  name?: string;
+  age?: number;
 }
 
-type Callback = () => void;
+const USERS_URL = 'http://localhost:3000/users/';
 
 export class User {
-	events: { [key: string]: Callback[] } = {};
-	private USERS_URL = 'http://localhost:3000/users/';
-
-	constructor(private data: UserProps) {}
-
-	get(propName: string): string | number {
-		return this.data[propName];
-	}
-
-	set(update: UserProps): void {
-		Object.assign(this.data, update);
-	}
-
-	on(eventName: string, callback: Callback) {
-		const handlers = this.events[eventName] || [];
-		handlers.push(callback);
-		this.events[eventName] = handlers;
-	}
-
-	trigger(eventName: string): void {
-		const handlers = this.events[eventName];
-
-		if (!handlers || handlers.length === 0) {
-			return;
-		}
-
-		handlers.forEach(callback => {
-			callback();
-		});
-	}
-
-	fetch(): void {
-		axios
-			.get(`${this.USERS_URL}/${this.get('id')}`)
-			.then((response: AxiosResponse) => {
-				this.set(response.data);
-			});
-	}
-
-	save(): void {
-		const id = this.get('id');
-		if (id) {
-			axios.put(`${this.USERS_URL}/${id}`, this.data);
-		} else {
-			axios.post(`${this.USERS_URL}`, this.data);
-		}
-	}
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(USERS_URL);
 }
