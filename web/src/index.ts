805a035ae24bf2ab1e329517fb7f8dfa0e22c18d
Adegoke Obasa <adegokeobasa@gmail.com>
2019-09-24 07:46:29 +0200

    create passthrough methods using getters

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,7 @@
 import {User} from './models/User';
+
+const user = new User({name: 'Jide Owosakin', age: 30});
+
+user.attributes.get('id');
+
+user.sync.save();
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,5 @@
 
+import {Attributes} from './Attributes';
 import {Eventing} from './Eventing';
 import {Sync} from './Sync';
 
@@ -13,4 +14,21 @@ const USERS_URL = 'http://localhost:3000/users/';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(USERS_URL);
+  public attributes: Attributes<UserProps>;
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.attributes.get;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
 }
