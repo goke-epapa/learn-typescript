b3102512366b182e05352fb52ab03aebf640f648
Adegoke <adegoke.obasa@deliveryhero.com>
2020-06-17 23:17:00 +0200

    add eventsMap and onButtonClick function

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,12 +1,24 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
-  template(): string{return `
+  eventsMap(): {[ key: string]: () => void} {
+    return {
+      'click:button': this.onButtonClick
+    }
+  }
+ 
+  onButtonClick(): void{
+    console.log('Hi there');
+  }
+
+  template(): string {
+    return `
 			<div>
 				<h1>User Form</h1>
 				<input/>
 			</div>
-		`}
+    `
+  }
 
   render(): void {
     const templateElement = document.createElement('template');
